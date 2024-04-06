c540fb501d62ab5141cc8f58b063fc28e48daba3
sr <tpfksghd95@gmail.com>
2024-04-06 18:52:20 +0900

    4/6 ServletWebServerFactory, DispatcherServlet도 bean으로 만들어서 등록하기

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,8 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,13 @@ import java.util.Objects;
 @RestController
 public class HelloController {
   private final HelloService helloService;
+  private final ApplicationContext applicationContext; //내부 기능을 수행할 때 사용할 거니깐 변수에다가 저장
 
-  public HelloController(HelloService helloService) {
+  public HelloController(HelloService helloService, ApplicationContext applicationContext) {
     this.helloService = helloService;
+    this.applicationContext = applicationContext;
+
+    System.out.println(applicationContext);
   }
 
   @GetMapping("/hello")
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,6 +12,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+  @Bean
+  public ServletWebServerFactory servletWebServerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
+
+  @Bean
+  public DispatcherServlet dispatcherServlet() {
+    return new DispatcherServlet();
+  }
 
   public static void main(String[] args) {
     AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -18,9 +28,12 @@ public class HellobootApplication {
       protected void onRefresh() {
         super.onRefresh();
 
-        ServletWebServerFactory sererFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = sererFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+        ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
+        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+        dispatcherServlet.setApplicationContext(this); //없어도 잘 동작됨
+
+        WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+          servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                   .addMapping("/*");
         });
         webServer.start();
