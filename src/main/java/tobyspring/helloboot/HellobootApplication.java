122954a658e9f0ac1eb3213b0db4a4ace73f090b
sr <tpfksghd95@gmail.com>
2024-04-13 16:56:22 +0900

    24.04.13 extract method

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,12 +1,10 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,23 +21,6 @@ public class HellobootApplication {
   }
 
   public static void main(String[] args) {
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-        dispatcherServlet.setApplicationContext(this); //없어도 잘 동작됨
-
-        WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                  .addMapping("/*");
-        });
-        webServer.start();
-      }
-    };
-    applicationContext.register(HellobootApplication.class); //bean 클래스 지정
-    applicationContext.refresh();
+    MySpringApplication.run(HellobootApplication.class, args);
   }
 }
