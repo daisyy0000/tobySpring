954e3c0ed3254f48b4bb5da8e02adcdfd718277c
sr <tpfksghd95@gmail.com>
2024-06-27 19:27:09 +0900

    24.06.27 메타 & 합성 어노테이션

---
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,17 @@
+package tobyspring.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) //TYPE: Class, Interface, Enum
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {
+
+}
