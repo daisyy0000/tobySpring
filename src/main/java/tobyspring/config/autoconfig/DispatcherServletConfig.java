637eb9e7a27596bdec0a625a2e83ad43bde54197
sr <tpfksghd95@gmail.com>
2024-08-01 17:30:00 +0900

    24.08.01. 동적인  자동  구성  정보 등록

--- src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -1,11 +1,11 @@
 package tobyspring.config.autoconfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
-public class DispatcherServerConfig {
+@MyAutoConfiguration
+public class DispatcherServletConfig {
   @Bean
   public DispatcherServlet dispatcherServlet() {
     return new DispatcherServlet();
