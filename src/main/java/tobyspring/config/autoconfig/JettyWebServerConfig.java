b7c6ac5839c692c326c79e4bb5029824a393b708
sr <tpfksghd95@gmail.com>
2024-08-02 17:49:12 +0900

    24.08.02. custom Conditional

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,28 +1,16 @@
 package tobyspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
   @Bean("jettyWebServerFactory")
   public ServletWebServerFactory servletWebServerFactory() {
     return new JettyServletWebServerFactory();
-  }
-
-  //이너클래스
-  static class JettyCondition implements Condition {
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return true; //서블릿 컨테이너는 제티로
-    }
   }
 }
