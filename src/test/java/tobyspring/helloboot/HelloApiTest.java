2d8ce73f23332adacbc1032d9e6b81e79ed19707
sr <tpfksghd95@gmail.com>
2024-08-10 18:06:30 +0900

    24.08.10. 스프링 부트의 자동 구성과 테스트로 전환

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -1,13 +1,16 @@
-import org.assertj.core.api.Assertions;
+package tobyspring.helloboot;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class HelloApiTest {
   @Test
   void helloApi() {
